960a8f39c8c320e23d7537a57324c95f6b7b1820
okunoyuki <yuki.okuno@jrits.ricoh.co.jp>
2016-11-23 11:06:14 +0900

    [GUI]ex11: Added a jar file.

--- src/gui/ex11/Clock.java
+++ src/gui/ex11/Clock.java
@@ -7,27 +7,30 @@ import java.awt.event.WindowEvent;
 public class Clock {
 	
 	public static void main(String[] args) {
+		start();
+	}
+	
+	protected static void start() {
 		MyFrame frame = new MyFrame();
-		frame.setVisible(true);
+		frame.setVisible(true);		
 	}
 
+	protected static class MyFrame extends Frame {
+		public MyFrame() {
+			super("Clock");
+			setSize(400, 300);
+			addWindowListener(new MyWindowAdaptor());
+		}
+	}
+
+	protected static class MyWindowAdaptor extends WindowAdapter {
+		public void windowOpened(WindowEvent event) {
+			System.out.println("Opened a window.");		
+		}
+		public void windowClosing(WindowEvent event) {
+			System.out.println("Closing the window.");
+			System.exit(0);
+		}
+	}
 }
 
-class MyFrame extends Frame {
-	public MyFrame() {
-		super();
-		setTitle("Clock");
-		setSize(400, 300);
-		addWindowListener(new MyWindowAdaptor());
-	}
-}
-
-class MyWindowAdaptor extends WindowAdapter {
-	public void windowOpened(WindowEvent event) {
-		System.out.println("Opened a window.");		
-	}
-	public void windowClosing(WindowEvent event) {
-		System.out.println("Closing the window.");
-		System.exit(0);
-	}
-}
