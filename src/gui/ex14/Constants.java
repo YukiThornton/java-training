d203d9c073f5e7c3384814eca5a6d6a3a880892b
okunoyuki <yukiokuno.rits@gmail.com>
2017-02-12 17:08:32 +0900

    [GUI] ex14: Completed preference function.

---
+++ src/gui/ex14/Constants.java
@@ -0,0 +1,21 @@
+package gui.ex14;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+public class Constants {
+	//public final static Dimension DEFAULT_FRAME_SIZE = new Dimension(400, 600);
+	public final static Dimension MINIMUN_FRAME_SIZE = new Dimension(275, 160);
+
+	public final static Dimension FIXED_DIALOG_SIZE = new Dimension(250, 300);
+	
+	public final static Font DEFAULT_FONT = new Font("arial black", Font.PLAIN, 24);
+	public final static int MINIMUM_FONT_SIZE = 10;
+	public final static int MAXIMUM_FONT_SIZE = 140;
+	
+	public final static Color DEFAULT_BACKGROUND_COLOR = new Color(230, 225, 225);
+	public final static Color DEFAULT_TEXT_COLOR_MAIN = new Color(77, 49, 49);
+	public final static Color DEFAULT_TEXT_COLOR_FOOTER = new Color(112, 107, 107);
+	public final static Color DEFAULT_TEXT_COLOR_SIDE = new Color(192, 108, 12);
+}
