aedbf273bebd231a9cdebdb1ddc7a0997095e9e4
okunoyuki <yukiokuno.rits@gmail.com>
2017-08-15 15:05:19 +0900

    [clock] Can edit clock max value and name.

--- src/clock/CountdownTimer.java
+++ src/clock/CountdownTimer.java
@@ -4,9 +4,14 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -19,13 +24,16 @@ public class CountdownTimer {
     private LocalDateTime startTime;
     private int passedSeconds;
     private int maxSeconds;
+    private String timerName;
     private boolean isActive = false;
     private ColorSet colorSet;
 
     private Label timerNameLabel;
+    private TextField timerNameTextField;
     private TimerChart chart;
     private Circle donutHole;
     private Label remainingMinuteLabel;
+    private TextField maxMinuteTxtField;
     private VBox timerNode;
 
     public enum UpdateCheckResult {
@@ -33,6 +41,7 @@ public class CountdownTimer {
     }
 
     public CountdownTimer(String timerName, int maxMinute, ColorSet colorSet) {
+        this.timerName = timerName;
         this.maxSeconds = maxMinute * 60;
         passedTimeInRound = Duration.of(0, ChronoUnit.SECONDS);
         passedTimeInTotal = Duration.of(0, ChronoUnit.SECONDS);
@@ -40,6 +49,41 @@ public class CountdownTimer {
         this.colorSet = colorSet;
 
         timerNameLabel = new Label(timerName);
+        timerNameLabel.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getClickCount() >= 2) {
+                    timerNameTextField.setText(CountdownTimer.this.timerName);
+                    timerNameTextField.setVisible(true);
+                }
+            }
+        });
+        timerNameTextField = new TextField(timerName);
+        timerNameTextField.setFont(new Font(20));
+        timerNameTextField.setMaxSize(150, 10);
+        timerNameTextField.setVisible(false);
+        timerNameTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch(event.getCode()) {
+                case ENTER:
+                    String val = validateTimerName(timerNameTextField.getText());
+                    if (val != null) {
+                        changeTimerName(val);
+                        timerNameTextField.setVisible(false);
+                    } else {
+                        timerNameTextField.setVisible(false);
+                    }
+                    break;
+                case ESCAPE:
+                    timerNameTextField.setVisible(false);
+                    break;
+                default:
+                    break;
+                }
+            }
+        });
+        StackPane donutTop = new StackPane(timerNameLabel, timerNameTextField);
 
         donutHole = new Circle(100, Color.WHITESMOKE);
         donutHole.setStrokeWidth(0);
@@ -48,10 +92,67 @@ public class CountdownTimer {
         remainingMinuteLabel = new Label(toRemainingText());
         remainingMinuteLabel.setTextFill(colorSet.remainingDimColor);
         remainingMinuteLabel.setFont(new Font(50));
-        StackPane chartPane = new StackPane(chart, donutHole, remainingMinuteLabel);
+        remainingMinuteLabel.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getClickCount() >= 2) {
+                    maxMinuteTxtField.setText(toTextInMinute(maxSeconds));
+                    maxMinuteTxtField.setVisible(true);
+                }
+            }
+        });
+        maxMinuteTxtField = new TextField(Integer.toString(maxMinute));
+        maxMinuteTxtField.setFont(new Font(20));
+        maxMinuteTxtField.setMaxSize(60, 10);
+        maxMinuteTxtField.setVisible(false);
+        maxMinuteTxtField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch(event.getCode()) {
+                case ENTER:
+                    int val = validateMaxMinute(maxMinuteTxtField.getText());
+                    if (val < 0) {
+                        maxMinuteTxtField.setVisible(false);
+                    } else {
+                        changeMaxMinute(val);
+                        maxMinuteTxtField.setVisible(false);
+                    }
+                    break;
+                case ESCAPE:
+                    maxMinuteTxtField.setVisible(false);
+                    break;
+                default:
+                    break;
+                }
+            }
+        });
+        StackPane donutCenter = new StackPane(remainingMinuteLabel, maxMinuteTxtField);
+        StackPane chartPane = new StackPane(chart, donutHole, donutCenter);
+        chartPane.setMaxSize(300, 300);
+        chartPane.setMinSize(300, 300);
 
-        timerNode = new VBox(timerNameLabel, chartPane);
+        timerNode = new VBox(donutTop, chartPane);
         timerNode.setAlignment(Pos.TOP_CENTER);
+    }
+
+    private int validateMaxMinute(String input) {
+        return Integer.parseInt(input);
+    }
+
+    private String validateTimerName(String input) {
+        return input;
+    }
+
+    private void changeTimerName(String newName) {
+        this.timerName = newName;
+        timerNameLabel.setText(timerName);
+    }
+
+    private void changeMaxMinute(int newValue) {
+        maxSeconds = newValue * 60;
+        int passed = passedSeconds();
+        int remaining = remainingSeconds(passed);
+        updateChartAndRemainingLabel(remaining, passed);
     }
 
     public Node getNode() {
@@ -106,7 +207,7 @@ public class CountdownTimer {
 
     private void updateChartAndRemainingLabel(int remaining, int passed) {
         chart.setTimeValues(remaining, passed);
-        remainingMinuteLabel.setText(toRemainingText(remaining));
+        remainingMinuteLabel.setText(toTextInMinute(remaining));
     }
 
     private void clearRoundVariable() {
@@ -124,10 +225,10 @@ public class CountdownTimer {
     }
 
     private String toRemainingText() {
-        return toRemainingText(remainingSeconds());
+        return toTextInMinute(remainingSeconds());
     }
 
-    private String toRemainingText(int remainingSeconds) {
+    private String toTextInMinute(int remainingSeconds) {
         return Integer.toString((int)Math.ceil(remainingSeconds / 60.0));
     }
 
